b3fa360d071c63dd69d84a92a60d8ed4a17ab2ab
forager-pixel <forager.pixel@gmail.com>
2024-12-20 12:14:25 -0500

    Fix error

--- dumptruck-dave/characters/dave/dave.gd
+++ dumptruck-dave/characters/dave/dave.gd
@@ -2,7 +2,8 @@ class_name Dave
 extends Character
 
 
-func get_nearest_candy_cane() -> Pickable
+func get_nearest_candy_cane() -> Pickable:
+	return null
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
