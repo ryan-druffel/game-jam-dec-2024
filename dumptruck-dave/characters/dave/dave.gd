9292989a276df846a22fdd057f749542179e5eb6
forager-pixel <forager.pixel@gmail.com>
2024-12-20 11:37:38 -0500

    Some initial scenes for Dave, Elves, World

---
+++ dumptruck-dave/characters/dave/dave.gd
@@ -0,0 +1,5 @@
+class_name Dave
+extends Character
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
---
+++ dumptruck-dave/characters/elf/elf.gd
@@ -0,0 +1,5 @@
+class_name Elf
+extends Character
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
