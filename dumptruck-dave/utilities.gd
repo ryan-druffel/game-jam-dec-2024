92337c79541c5339e3f5b32160680b551a2c7200
Ryan Druffel <thefriendlynextdoorneighbor@gmail.com>
2024-12-21 15:29:34 -0500

    Utility for scaling stuff vertically

--- dumptruck-dave/utilities.gd
+++ dumptruck-dave/utilities.gd
@@ -2,6 +2,11 @@ class_name Utilities
 
 const MAX_FOLLOW_SPEED : float = 20;
 
+# Iso stuff
+const MIN_SCALE	: float =	0.5;
+const MAX_SCALE	: float =	2;
+const SCREEN_SIZE_Y : float = 360;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,3 +28,8 @@ static func logSmoothVector2(target : Vector2, current : Vector2, factor : float
 static func smoothedMoveCarryThing(target : Vector2, current : Vector2):
 	var change : Vector2 = Utilities.logSmoothVector2(target, current, 0.05);
 	return change if change.length() < MAX_FOLLOW_SPEED else MAX_FOLLOW_SPEED * (target - current);
+	
+static func get_scale_with_screen_factor(vert_screen_position : float) -> float:
+	var screen_frac : float = (vert_screen_position / SCREEN_SIZE_Y);
+	var scale_to : float = screen_frac * (MAX_SCALE - MIN_SCALE) + MIN_SCALE
+	return scale_to
--- dumptruck-dave/world/make_parent_look_iso.gd
+++ dumptruck-dave/world/make_parent_look_iso.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 @export var scale_this : Node2D;
 
-const MIN_SCALE	: float =	0.5;
-const MAX_SCALE	: float =	2;
-
-const SCREEN_SIZE_Y : float = 360;
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,6 +10,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if scale_this:
-		var screen_frac : float = ((scale_this.get_global_transform_with_canvas().get_origin().y) / SCREEN_SIZE_Y);
-		var scale_to : float = screen_frac * (MAX_SCALE - MIN_SCALE) + MIN_SCALE
+		var scale_to : float = Utilities.get_scale_with_screen_factor(scale_this.get_global_transform_with_canvas().get_origin().y)
 		scale_this.scale = (Vector2(scale_to, scale_to))
